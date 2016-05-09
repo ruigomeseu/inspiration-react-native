1bfa4eee29d2d2cf8ff59eda492ccbd0625e56b8
Rui Gomes <hello@ruigomes.me>
2016-05-09 18:56:21 +0100

    Share button

--- index.ios.js
+++ index.ios.js
@@ -10,20 +10,56 @@ import React, {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableHighlight,
 } from 'react-native';
 
 var Spinner = require('react-native-spinkit');
+
+const FBSDK = require('react-native-fbsdk');
+const {
+  ShareDialog,
+  ShareButton
+} = FBSDK;
 
 var REQUEST_URL = 'http://inspiration-ruigomes.rhcloud.com/quotes'
 
 class Inspiration extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       quotes: null,
-      selectedQuote: 0
+      selectedQuote: 0,
+      shareLinkContent: {
+        contentType: 'link',
+        contentDescription: 'Best memes online',
+        contentUrl: "https://www.facebook.com/",
+      },
     };
+  }
+
+  shareLinkWithShareDialog() {
+    var tmp = this;
+    ShareDialog.canShow(this.state.shareLinkContent).then(
+      function(canShow) {
+        if (canShow) {
+          return ShareDialog.show(tmp.state.shareLinkContent);
+        }
+      }
+    ).then(
+      function(result) {
+        if (result.isCancelled) {
+          alert('Share cancelled');
+        } else {
+          alert('Share success with postId: '
+            + result.postId);
+        }
+      },
+      function(error) {
+        alert('Share fail with error: ' + error);
+      }
+    );
   }
 
   clickMe() {
@@ -40,19 +76,22 @@ class Inspiration extends Component {
     var quote = this.state.quotes[this.state.selectedQuote];
 
     return (
-      <TouchableOpacity  style={styles.container} onPress={this.clickMe.bind(this)}>
-        <View style={styles.container}>
-          <Text style={styles.quote}>
-            { quote.content }
-          </Text>
-          <Text style={styles.author}>
-            — { quote.author }
-          </Text>
-          <Text style={styles.authorinfo}>
-            { quote['author-info'] }
-          </Text>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.quoteContainer} onPress={this.clickMe.bind(this)}>
+            <Text style={styles.quote}>
+              { quote.content }
+            </Text>
+            <Text style={styles.author}>
+              — { quote.author }
+            </Text>
+            <Text style={styles.authorinfo}>
+              { quote['author-info'] }
+            </Text>
+        </TouchableOpacity>
+        <View style={styles.shareContainer}>
+          <ShareButton shareContent={this.state.shareLinkContent}/>
         </View>
-      </TouchableOpacity>
+      </View>
     );
   }
 
@@ -83,8 +122,11 @@ class Inspiration extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     backgroundColor: '#2196F3',
+  },
+  quoteContainer: {
+    flex: 1,
+    justifyContent: 'center',
   },
   spinnercontainer: {
     flex: 1,
@@ -92,8 +134,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#2196F3',
     alignItems: 'center',
   },
+  shareContainer: {
+    height:50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   quote: {
-    textAlign: 'left',
+    textAlign: 'center',
     color: '#FFFFFF',
     fontSize: 40,
     paddingLeft: 30,
